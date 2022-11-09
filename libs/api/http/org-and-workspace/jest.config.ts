7d95d3aee65a2668caa6a6b4bd911b06fe89f45c
Igor Katsuba <igor@katsuba.dev>
2022-11-09 21:48:25 +0400

    moves http modules

---
+++ libs/api/http/org-and-workspace/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'api-http-org-and-workspace',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/api/http/org-and-workspace',
+};
---
+++ libs/api/http/report-client-error/README.md
@@ -0,0 +1,7 @@
+# api-http-report-client-error
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test api-http-report-client-error` to execute the unit tests via [Jest](https://jestjs.io).
