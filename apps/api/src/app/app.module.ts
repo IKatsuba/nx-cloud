4a91f54ff9c5f86fd76cec368ddbfde0b146c26a
Igor Katsuba <igor@katsuba.dev>
2022-11-09 22:26:48 +0400

    adds the firebase storage

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { ApiOrgAndWorkspaceModule } from '@nx-cloud/api/http/org-and-workspace';
 import { ApiRunsModule } from '@nx-cloud/api/http/runs';
 import { ApiSaveMetricsModule } from '@nx-cloud/api/http/save-metrics';
 import { ApiReportClientErrorModule } from '@nx-cloud/api/http/report-client-error';
-import { S3StorageModule } from '@nx-cloud/api/storage';
+import { FirebaseStorageModule } from '@nx-cloud/api/storage';
 
 @Module({
   imports: [
     ApiOrgAndWorkspaceModule,
-    ApiRunsModule.forRoot({ imports: [S3StorageModule] }),
+    ApiRunsModule.forRoot({ imports: [FirebaseStorageModule] }),
     ApiSaveMetricsModule,
     ApiReportClientErrorModule,
   ],
--- libs/api/storage/src/index.ts
+++ libs/api/storage/src/index.ts
@@ -1,2 +1,5 @@
+export * from './lib/firebase/firebase-storage.service';
+export * from './lib/firebase/firebase-storage.module';
+
 export * from './lib/s3/s3-storage.module';
 export * from './lib/storage';
