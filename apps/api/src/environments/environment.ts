bb4bd95202163afa938b84a1bf9d11da47e93d8f
IKatsuba <igor@katsuba.dev>
2023-02-20 10:19:05 +0400

    dev environment example

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -4,12 +4,17 @@ import {
   TaskEntity,
   WorkspaceEntity,
 } from '@nx-cloud/api/db/entities';
+import * as process from 'process';
 
 export const environment = {
   production: false,
   mikroOrm: {
     type: 'postgresql' as const,
-    clientUrl: 'postgresql://localhost:5432/postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+    dbName: process.env.POSTGRES_DB || 'nx_cloud',
+    password: process.env.POSTGRES_PASSWORD || null,
+    user: process.env.POSTGRESS_USER || null,
     metadataProvider: ReflectMetadataProvider,
     entities: [WorkspaceEntity, TaskEntity, ExecutionEntity],
   },
