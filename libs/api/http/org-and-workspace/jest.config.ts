36e3b9446b0f7d096640c83ab2503d8f57796851
IKatsuba <igor@katsuba.dev>
2023-03-03 23:11:39 +0200

    nx bump

--- libs/api/http/org-and-workspace/jest.config.ts
+++ libs/api/http/org-and-workspace/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'api-http-org-and-workspace',
   preset: '../../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/api/http/org-and-workspace',
