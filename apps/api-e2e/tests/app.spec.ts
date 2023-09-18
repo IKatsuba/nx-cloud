57441980fc90ef6bb58aaac815efb3e74fe8f0c9
IKatsuba <igor@katsuba.dev>
2023-09-18 22:31:16 +0300

    test: enable e2e tests

--- apps/api-e2e/tests/app.spec.ts
+++ apps/api-e2e/tests/app.spec.ts
@@ -1,44 +1,41 @@
-import { readJson, runNxCommandAsync } from '@nx/plugin/testing';
-import { addPackageToPackageJson, createWorkspace } from './utils';
+import { readJson, runNxCommandAsync, tmpProjPath } from '@nx/plugin/testing';
+import { addPackageToPackageJson, createTestProject } from './utils';
+import { rmSync } from 'fs';
+import { getPackageManagerCommand } from 'nx/src/utils/package-manager';
+import { execSync } from 'child_process';
 
 describe('api e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
   beforeAll(async () => {
-    createWorkspace();
-    addPackageToPackageJson('@nrwl/nx-cloud');
-    // const project = uniq('app');
-    // await runNxCommandAsync(`generate @nx/node:application ${project}`);
+    createTestProject();
+
+    addPackageToPackageJson('nx-cloud');
   }, 120000);
 
   afterAll(() => {
-    // `nx reset` kills the daemon, and performs
-    // some work which can help clean up e2e leftovers
-    runNxCommandAsync('reset');
+    rmSync(tmpProjPath(), {
+      recursive: true,
+      force: true,
+    });
   });
 
   it('should configure Nx Cloud', async () => {
-    await runNxCommandAsync(`generate @nrwl/nx-cloud:init`, {
+    execSync(`npx nx generate nx-cloud:init`, {
       env: {
         ...process.env,
-        NX_CLOUD_API: 'http://127.0.0.1:3000/',
+        NX_CLOUD_API: 'http://localhost:3000/',
       },
-      silenceError: false,
+      cwd: tmpProjPath(),
     });
 
     const nxJson = readJson('nx.json');
 
     expect(nxJson.tasksRunnerOptions).toEqual({
       default: {
-        runner: '@nrwl/nx-cloud',
+        runner: 'nx-cloud',
         options: {
           cacheableOperations: ['build', 'lint', 'test', 'e2e'],
           accessToken: expect.any(String),
-          url: 'http://127.0.0.1:3000/',
+          url: 'http://localhost:3000/',
         },
       },
     });
